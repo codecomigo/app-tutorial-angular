6bc53e3b8ed4ecd2ff0c05c636872fc274e8c4e5
Andressa Silva <code.andressa@gmail.com>
2024-01-02 20:02:51 -0300

    Adding a routerlink directive to go back to home page clicking the homes logo and adding a form to create forms from details page (incomplete)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule, RouterOutlet, HomeComponent, ],
+  imports: [RouterModule, CommonModule, RouterOutlet, HomeComponent,],
   template: `
   <main>
+  <a [routerLink]="['/']">
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
     </header>
+  </a>
     <section class="content">
     <router-outlet></router-outlet>
     </section>
