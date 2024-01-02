d434e4b7b87abc3ec43da28cb5e422288e263913
Andressa Silva <code.andressa@gmail.com>
2024-01-02 14:35:07 -0300

    Step two angular tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `<h1>Hello world!</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-tutorial';
+  title = 'homes';
 }
